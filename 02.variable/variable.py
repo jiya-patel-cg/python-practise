fd9ba7292ff4be37331dceaadaa41fbe7e83d9de
Jiya-patel-cg <jiya.patel.cg@gmail.com>
2026-08-24 17:02:13 +0530

    files updated

---
+++ 02.variable/variable.py
@@ -0,0 +1,39 @@
+name = "Jiya"
+age = 18
+city = "Patan"
+
+student_name = "Jiya patel"
+student_roll_number = 29
+student_branch = "Computer Science"
+
+marks = 90
+print("90")
+
+marks = 94
+print("94")
+
+name,age,city = "Jiya",18,"Patan"
+print(name,age,city)
+
+x=y=z=0
+print(x,y,z)
+
+student_name= "Jiya"
+student_full_name= "Jiya"
+student_class= "B.Teach"
+
+#store and update student's information
+student_name="Jiya"
+student_age= 18
+print("student name:", student_name)
+print("student age:", student_age)
+
+age_int=18
+Age_float=19.0
+aGe_string="18"
+AgE_boolean=True
+result_nonetype=None
+print(type(Age_float))
+print(type(aGe_string))
+print(type(AgE_boolean))
+print(type(result_nonetype))
