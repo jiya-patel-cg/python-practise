bbfff5fe0babd35e5a95e372ba45d1053befe82b
Jiya-patel-cg <jiya.patel.cg@gmail.com>
2026-08-25 10:12:20 +0530

    added a file

---
+++ 03.data types/datatypes.py
@@ -0,0 +1,70 @@
+age_int=18
+Age_float=19.0
+aGe_string="18"
+AgE_boolean=True
+result_nonetype=None
+print(type(Age_float))
+print(type(aGe_string))
+print(type(AgE_boolean))
+print(type(result_nonetype))
+
+
+#Que-24
+
+student_name="rahul"
+student_age=20
+student_height=5.5
+Is_the_person_a_student=True
+student_result=None
+
+#Que-25
+
+a=50
+b=50.0
+c="50"
+print(type(a))
+print(type(b))
+print(type(c))
+
+a=True
+b="True"
+print(type(a)) #because it is a bollean type
+print(type(b)) #because it is a string type
+
+a=None #because it is a none type
+b="None" #because it is a string type
+print(type(a))
+print(type(b))
+
+value=29
+print(type(value))
+value1 = str(value)
+print(type(value1))
+
+#29 (info about product)
+product_name="headphone"
+product_quantity=3
+product_price=615.50
+product_availibity=True
+product_discount=None
+print(type(product_name))
+print(type(product_quantity))
+print(type(product_price))
+print(type(product_availibity))
+print(type(product_discount))
+
+#Que30
+age=10
+Age=10.0
+aGe="10"
+possibal=True
+possible="True"
+availbilty=None
+availbilty="None"
+print(type(age))
+print(type(Age))
+print(type(aGe))
+print(type(possibal))
+print(type(possible))
+print(type(availbilty))
+print(type(availbilty))
