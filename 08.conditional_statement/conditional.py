9a8b8f09ed8dcfb41da436d142a0db11dc0739fd
Jiya-patel-cg <jiya.patel.cg@gmail.com>
2026-09-07 09:34:17 +0530

    add file

---
+++ 08.conditional_statement/conditional.py
@@ -0,0 +1,184 @@
+a=int(input("Enter your age :-"))
+if a >= 18:
+    print(" Adult")
+else:
+    print("Minor")
+a=int(input("Enter your age :-"))
+if a >= 18:
+    print(" Adult")
+else:
+    print("Minor")
+que7
+a=int(input("Enter an integer:-"))
+if a%2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+que8
+marks=int(input("Enter marks:-"))
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
+que9
+a=float(input("Enter any number:-"))
+b=float(input("Enter any number:-"))
+if a > b:
+    print("A is greater")
+else:
+    print("B is greater")
+que10
+a=int(input("Enter your makrs:-"))
+if a >= 90:
+    print("A")
+elif a >= 75:
+    print("B")
+elif a >= 60:
+    print("C")
+elif a >= 40:
+    print("D")
+else:
+    print("F")
+que11
+a=int(input("Enter any integer:-"))
+if a > 0: 
+    print("Positive")
+elif a < 0:
+    print("Negative")
+else:
+    print("Zero")
+que12
+a=int(input("Enter an integer:-"))
+if a == 1:
+    print("Monday")
+elif a == 2:
+    print("Tuesday")
+elif a == 3:
+    print("Wednesday")
+elif a == 4:
+    print("Thursday")
+elif a == 5:
+    print("Friday")
+elif a==6:
+    print("Saturday")
+else:
+    print("Sunday")
+que13
+a=int(input("Enter your makrs:-"))
+if a >= 90:
+    print("Excellent")
+elif a >= 75:
+    print("Good")
+elif a >= 40:
+    print("Pass")
+
+else:
+    print("Fail")
+que14
+a=int(input("Enter an integer:-"))
+if a == 1:
+    print("1")
+elif a == 2:
+    print("2")
+elif a == 3:
+    print("3")
+else:
+    print("Other")
+que15
+a=int(input("Enter your age:-"))
+if a >= 18:
+    if a <= 60:
+        print("Eligible")
+que16
+marks = int(input("Enter marks: "))
+
+if marks >= 40:
+    if marks >= 75:
+        print("Good")
+    else:
+        print("Passed")
+else:
+    print("Failed")
+que16
+marks = int(input("Enter marks: "))
+
+if marks >= 40:
+    if marks >= 75:
+        print("Good")
+    else:
+        print("Passed")
+else:
+    print("Failed")
+que17
+a=int(input("Enter an integer:-"))
+if a > 0:
+    if a >= 100:
+        print("Greater than 100")
+    else:
+        print("Positive")
+else:
+    print("Negative")
+que18
+a=int(input("Enter your age:- "))
+if a>= 18:
+    if a <= 60:
+        print("You are eligible")
+    else:
+        print("You are not eligible becz u are over 60")
+else:
+    print("You are not eligible becz u are a minor")
+que19
+a=int(input("Enter an integer:-"))
+if a != 0:
+    if a > 0:
+        print("Positive")
+    else:
+        print("Negative")
+else:
+    print("Zero")
+que20
+marks=int(input("Enter your marks:-"))
+age=int(input("Enter your age:-"))
+if marks >= 40 and age >= 18:
+    print("Candidate is eligible")
+else:
+    print("Not eligible")
+que21
+a= int(input("Enter any integer:-"))
+if a<10 or a>100:
+    print("Special")
+que22
+age=int(input("Enter your age:- "))
+has_id:True 
+
+
+if age>=18 and "has_id":
+    print("Allowed")
+que23
+first_number=int(input("Enter any number:-"))
+second_number=int(input("Enter any number:-"))
+if first_number>10 and second_number>10: 
+    print("Both are greater than 10")
+que24
+a=int(input("Enter an integer:-"))
+if a<0 or a>100:
+    print("Done")
+que25
+is_closed = False
+
+if not is_closed:
+    print("Open")
+que26
+a=int(input("Enter an integer:-"))
+if a>10 and  a<50 :
+    print("It is between 10-50")
+que27
+a=int(input("Enter an integer:-"))
+if a<10 or a>50:
+    print("It is not between 10-50")
+que28
+is_student=False
+has_id=False
+has_ticket=False
+if not is_student or has_id or has_ticket: 
+                                        
