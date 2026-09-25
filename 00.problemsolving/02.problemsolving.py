56fe9d3ba14e2bea09952a23e52dd4db8335a97c
Jiya-patel-cg <jiya.patel.cg@gmail.com>
2026-09-25 09:32:55 +0530

    add a file

---
+++ 00.problemsolving/02.problemsolving.py
@@ -0,0 +1,155 @@
+#que1
+# for row in range(3):
+#     for column in range(3):
+#         print("*", end="")
+#     print()
+
+#que2
+# for row in range(1,4):
+#     for column in range(1,4):
+#         print(column, end="")
+#     print()
+
+#que3
+# for row in range(1,4):
+#     for column in range(1,4):
+#         print(row, end="")
+#     print()
+
+#que4
+# for row in range(1, 6):
+#     for column in range(1,row+1):
+#         print("*", end="  ")
+#     print("")
+
+#que5
+# for row in range(1, 6):
+#     for column in range(5,row-1,-1):
+#         print("*", end="  ")
+#     print("")
+
+#que6
+# for row in range(1,6):
+#     for column in range(1,row+1):
+#         print(column, end=" ")
+#     print()
+
+#que7
+# for row in range(1,6):
+#     for column in range(1,row+1):
+#         print(row, end=" ")
+#     print()
+
+#que8
+# for row in range(1,6):
+#     for column in range(1,11):
+#         print(row*column,end=" ")
+#     print()
+
+#que9
+# for row in range(1,4):
+#     for column in range(1,6):
+#         print(row*column,end=" ")
+#     print()
+
+#que10
+# for row in range(1,6):
+#     for column in range(1,6):
+#         print(column**2,end="  ")
+#     print()
+
+#que11
+# for row in range(1,6):
+#     for column in range(1,row+1):
+#         print(chr(64+column), end="  ")
+#     print("")
+
+# #que12
+# for row in range(1,6):
+#     for column in range(1,row+1):
+#         print(chr(64+row), end="  ")
+#     print("")
+
+#que13
+# for row in range(1,10,2):
+#     for column in range(1,row+1,2):
+#         print(column, end="  ")
+#     print("")
+
+# #que14
+# for row in range(2,12,2):
+#     for column in range(2,row+1,2):
+#         print(column, end="  ")
+#     print("")
+#que15
+# for row in range(5):
+# #     for column in range(5):
+# #         print("*", end="")
+# #     print()
+# que16
+# for row in range(1,6):
+#     for column in range(1,6):
+#         print(column, end=" ")
+#     print()
+
+# #que17
+# n=int(input("enter a number: "))
+# for i in range(3):
+#     for j in range(3):
+#         print(n,end=" ")
+#         n+=1
+#     print()
+
+# # 18
+# n=int(input("enter a number: "))
+# for i in range(4):
+#     for j in range(5):
+#         print(n,end=" ")
+#         n+=1
+#     print()
+
+# # 19
+# for i in range(1,4):
+#     for j in range(1,4):
+#         a = (i,j)
+#         print(a,end=" ")
+#     print()
+
+# # 20
+for i in range(1,5):
+    for j in range(1,5):
+        a = i,j
+        print(a)
+
+# # 21
+for i in range(1,11):
+    for j in range(1,11):
+        print(i*j, end=" ")
+    print()
+
+# # 22
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+
+# # 23
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+# # 24
+# for i in range(1,6):
+#     for j in range(5,i-1,-1):
+#         print(j,end="")
+#     print()
+
+# # 25
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(i,end="")
+#     print()
+
+
+
